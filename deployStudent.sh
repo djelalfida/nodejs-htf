c9663ce556b88aabe468485396494dddc73e2e48
Robin Peerlinck <robinpeerlinck@gmail.com>
2022-11-14 16:44:46 +0100

    Added Deploy Scripts

---
+++ deployStudent.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+STACK_NAME=HTF22-<TeamNameHere>
+MY_REGION=eu-west-1
+MY_DEV_BUCKET=htf-22-i8c-bucket
+
+AWS_PROFILE=default
+
+# Package the cloudformation package
+aws cloudformation package --template ./cfn-students.yaml --s3-bucket $MY_DEV_BUCKET --output-template ./cfn-students-export.yaml
+
+# Deploy the package
+sam deploy --template-file ./cfn-students-export.yaml --stack-name $STACK_NAME --capabilities CAPABILITY_NAMED_IAM
